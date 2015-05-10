b7c721086b34c20fa53981a53922c98a87686a9e
Serkan <damian.foyle@gmail.com>
2015-05-10 17:29:58 +0300

    DB: Trigger

---
+++ src/db_triggers.py
@@ -0,0 +1,19 @@
+"""
+    db_triggers.py
+    ~~~~~~~~~~~~~~
+
+    :aciklama:
+        Veritabanina veri girisi ve gerekli triggerlar icin
+
+    :yazar: github.com/serong
+"""
+
+import sqlite3
+
+
+class SayisalDBT(object):
+
+    def __init__(self):
+        self.db_name = "sayisal.db"
+
+
